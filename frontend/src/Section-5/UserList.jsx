2ff05b774f3ffe7db7e1213889e3b586f6842976
dev10 <dev10>
2025-09-08 12:37:21 +0530

    AK Singh: Work for use validation and section 1(ii).

--- frontend/src/Section-5/UserList.jsx
+++ frontend/src/Section-5/UserList.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm,Controller } from "react-hook-form";
 import axios from "axios";
 import { motion } from "framer-motion";
 import useTableApi from "../Utils/CustomHooks/useTableApi";
 import { useParams } from "../Utils/CustomHooks/useParams";
-import useValidation from "../Utils/useValidation";
+import useValidation from "../Utils/CustomHooks/useValidation";
+import dayjs from "dayjs";
 import {
   Accordion,
   AccordionSummary,
@@ -26,22 +27,26 @@ import {
 } from "@mui/material";
 
 const USLogForm = () => {
-  //   const [form, setForm] = useState({
-  //     aircraft_master_id: "",
-  //     airframe_hrs: "",
-  //     reason_for_placing_unserviceable: "",
-  //     system_time_date: "",
-  //   });
+  const { params, loading } = useParams();
+  const { data: howFound, loading: loadingHowFound } =
+    useTableApi("how_found_defects");
+  const { data: entryType, loading: loadingEntryTypes } =
+    useTableApi("entry_types");
+  const { data: aircraftMasters, loading: loadingMasters } = useTableApi(
+    "aircraft_masters",
+    { id: params.aircraft_master_id },
+  );
+
   const { formData, errors, handleChange, validateAll, setFormData } =
     useValidation(
       {
         entryType: "",
         howFound: "",
         dateAndTime: "",
-        airframeHrs: "",
-        aircraft_master_id: "",
+        airframeHrs: 405,
+        aircraft_master_id: params ? params.aircraft_master_id : "",
         reason_for_placing_unserviceable: "",
-        system_time_date: "",
+        system_time_date: dayjs().format("YYYY-MM-DD HH:mm:ss"),
       },
       {
         entryType: { required: true },
@@ -54,40 +59,10 @@ const USLogForm = () => {
       },
     );
   const { register, watch } = useForm();
-  const { params, loading3 } = useParams();
-  const { data: howFound, loading } = useTableApi("how_found_defects");
-  const { data: entryType, loading1 } = useTableApi("entry_types");
-  //   const { data: aircraftMaster, loading2 } = useTableApi("aircraft_masters", {
-  //     id: params.aircraft_master_id,
-  //   });
 
   const [user, setUser] = useState({ name: "", rank: "" });
-  const [afHours, setAfHours] = useState("");
   const authCode = watch("authCode");
   const [open, setOpen] = useState(false);
-
-  const [aircraftMaster, setAircraftMaster] = useState(null);
-  useEffect(() => {
-    if (!loading3) {
-      const aircraft_master_id = params.aircraft_master_id;
-      if (aircraft_master_id) {
-        axios
-          .get(`/api/leadingParticularsOfAircraft/${aircraft_master_id}`)
-          .then((response) => {
-            setAircraftMaster(response.data);
-          })
-          .catch((error) => {
-            console.error("Error aircraft Marks:", error);
-          });
-      }
-    }
-  }, [params, loading3]);
-
-  useEffect(() => {
-    if (aircraftMaster) {
-      setFormData((prev) => ({ airframeHrs: aircraftMaster.airframe_hrs }));
-    }
-  }, [aircraftMaster, setFormData]);
 
   useEffect(() => {
     if (authCode === "1234") {
@@ -105,97 +80,76 @@ const USLogForm = () => {
   //     setValue("timestamp", nowLocal);
   //   }, [setValue, howFound, entryType, loading, loading2]);
 
-  //      const handleChange = (e) => {
-  //        setFormData({
-  //            ...formData,
-  //            [e.target.name]:e.target.value
-  //        });
-  //     };
-
+  //   const handleChange = (e) => {
+  //     console.log(formData);
+  //   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const payload = {
-      aircraft_master_id: formData.aircraft_master_id,
-      airframe_hrs: formData.airframeHrs,
-      reason_for_placing_unserviceable:
-        formData.reason_for_placing_unserviceable,
-      system_time_date: formData.system_time_date,
-    };
-    if (validateAll()) {
-      try {
-        const res = await fetch("/api/serviceability-log/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(payload),
-        });
-
-        if (!res.ok) throw new Error("Failed to save data");
-        const data = await res.json();
-        console.log("Saved:", data);
-      } catch (err) {
-        console.error(err);
-      }
-    } else {
-      console.log("Blundeeeeer");
-    }
+    console.log("formData:", formData);
+    //     const payload = {
+    //       aircraft_master_id: formData.aircraft_master_id,
+    //       airframe_hrs: formData.airframeHrs,
+    //       reason_for_placing_unserviceable:
+    //         formData.reason_for_placing_unserviceable,
+    //       system_time_date: formData.system_time_date,
+    //     };
+    //     if (validateAll()) {
+    //       try {
+    //         const res = await fetch("/api/serviceability-log/", {
+    //           method: "POST",
+    //           headers: {
+    //             "Content-Type": "application/json",
+    //           },
+    //           body: JSON.stringify(payload),
+    //         });
+    //
+    //         if (!res.ok) throw new Error("Failed to save data");
+    //         const data = await res.json();
+    //         console.log("Saved:", data);
+    //       } catch (err) {
+    //         console.error(err);
+    //       }
+    //     } else {
+    //       console.log("Blundeeeeer");
+    //     }
   };
 
-  const onSubmit = (data) => {
-    console.log(data);
-    alert("Submitted successfully!");
-  };
   //   const FloatingLabel = ({ label }, { label: string }) => (
   const FloatingLabel = ({ label }) => (
     <div className="absolute left-0 -top-4 text-sm text-gray-600 transition-all peer-placeholder-shown:top-3.5 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-focus:-top-4 peer-focus:text-sm peer-focus:text-indigo-500">
       {label}
     </div>
   );
-  const checkBoxes = ["Limitation", "Deferred", "Husbandry", "Concession"];
-  if (loading && loading1) {
-    <p>Loading...</p>;
-  }
+  const checkBoxesLDHC = ["Limitation", "Deferred", "Husbandry"];
+  const checkBoxesChecks = ["Independent Check", "Loose Articles Check"];
 
   return (
-    <div className="items-start bg-blue-300 p-1">
+    <div className="items-start bg-blue-200 p-1">
       {/*<h1>------------------------------------------------------Headings ------------------------------------------------------------------</h1>*/}
-      <div className="rounded-lg bg-gradient-to-r from-[#FFE6CC] via-[#87CEEB]/60 to-[#FFD5E0] h-12 mt-1 mb-1">
+      <div className="rounded-lg bg-gradient-to-r from-[#FFE6CC] via-[#87CEEB]/60 to-[#FFD5E0] h-16 mt-1 mb-1">
         <h2
-          className=" absolute font-serif text-md font-bold"
+          className="absolute text-md font-bold"
           style={{
             position: "absolute",
             left: "50%",
             transform: "translateX(-50%)",
-            fontSize: "25px",
-            margin: 0,
-            fontFamily: "sans-serif",
+            fontSize: "35px",
+            margin: 4,
+            fontFamily: "Algerian",
           }}
         >
           CHANGE OF SERVICEABILITY LOG
         </h2>
-        <h2
-          style={{
-            marginLeft: "90%",
-            fontWeight: "bold",
-            fontSize: "15px",
-            color: "crimson",
-            margin: 0,
-          }}
-        >
-          MOD Form 707
-        </h2>
       </div>
       {/*<h1>-------------------------------------------------------Forms & Body ---------------------------------------------------</h1>*/}
       <form onSubmit={handleSubmit} className=" p-1 space-y-4 ">
-        <div className="border border-green-100 bg-gradient-to-br from-yellow-50 via-green-50 to-blue-50 rounded-lg shadow-inner p-4">
-          <h2 className="text-lg aligned-center font-semibold text-gray-800 mb-4">
-            General Information :-
-          </h2>
+        <div className="p-4 rounded-lg bg-white/20 backdrop-blur-md border border-white/90 shadow-lg">
           {/*<h1>-------------------------------------------------------row 1 ---------------------------------------------------</h1>*/}
-          <div className="grid grid-cols-4 gap-4">
+          <div className="grid md:grid-cols-4 sm:grid-cols-2 gap-4">
             <div>
-              <label className="block mb-1 font-medium">Entry Type</label>
+              <label className="inline-block px-2 py-1 rounded-full text-blue-900 font-semibold hover:bg-blue-300 transition">
+                Entry Type
+              </label>
               <select
                 name="entryType"
                 value={formData.entryType}
@@ -203,7 +157,7 @@ const USLogForm = () => {
                 className="border p-2  w-full rounded border-gray-300 bg-transparent text-gray-800 focus:outline-none focus:border-indigo-500"
               >
                 <option value="">Select Entry Type</option>
-                {!loading &&
+                {!loadingEntryTypes &&
                   entryType?.map((entry_types) => (
                     <option key={entry_types.id} value={entry_types.occasion}>
                       {entry_types.occasion}
@@ -218,7 +172,9 @@ const USLogForm = () => {
             </div>
 
             <div>
-              <label className="block mb-1 font-medium">How Found</label>
+              <label className="inline-block px-2 py-1 rounded-full text-blue-900 font-semibold hover:bg-blue-300 transition">
+                How Found
+              </label>
               <select
                 name="howFound"
                 value={formData.howFound}
@@ -226,7 +182,7 @@ const USLogForm = () => {
                 className="border p-2  w-full rounded border-gray-300 bg-transparent text-gray-800 focus:outline-none focus:border-indigo-500"
               >
                 <option value="">Select How Found</option>
-                {!loading1 &&
+                {!loadingHowFound &&
                   howFound?.map((how_found) => (
                     <option key={how_found.id} value={how_found.occasion}>
                       {how_found.occasion}
@@ -241,7 +197,9 @@ const USLogForm = () => {
             </div>
 
             <div>
-              <label className="block mb-1 font-medium">Date & Time</label>
+              <label className="inline-block px-2 py-1 rounded-full text-blue-900 font-semibold hover:bg-blue-300 transition">
+                Date & Time
+              </label>
               <input
                 type="datetime-local"
                 //                 type="date"
@@ -257,13 +215,15 @@ const USLogForm = () => {
               )}
             </div>
 
-            {aircraftMaster && (
+            {aircraftMasters && (
               <div>
-                <label className="block mb-1 font-medium">Airframe Hours</label>
+                <label className="inline-block px-2 py-1 rounded-full text-blue-900 font-semibold hover:bg-blue-300 transition">
+                  Airframe Hours
+                </label>
                 <input
                   type="text"
                   name="airframe_hrs"
-                  value={formData.airframeHrs || "40"}
+                  value={formData.airframeHrs}
                   className="border p-2  w-full rounded border-gray-300 bg-transparent text-gray-800 focus:outline-none focus:border-indigo-500"
                   readOnly
                 ></input>
@@ -272,11 +232,10 @@ const USLogForm = () => {
           </div>
           {/* SECTION 2: REASON & CONDITIONS */}
           {/*<h1>------------------------------------------------------- row 2 ---------------------------------------------------</h1>*/}
-
-          <div className="space-y-6">
+          <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-2 mt-8">
             {/* Reason */}
-            <div>
-              <label className="block mb-1 font-medium">
+            <div className="border-2 border-black-600 rounded-lg p-4">
+              <label className="inline-block px-2 py-1 rounded-full text-blue-900 font-semibold hover:bg-blue-300 transition">
                 Reason for placing aircraft unserviceable
               </label>
               <textarea
@@ -295,18 +254,42 @@ const USLogForm = () => {
               )}
             </div>
 
-            {/* Checkboxes */}
-            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-              {checkBoxes.map((box) => (
-                <label key={box} className="flex items-center space-x-2">
-                  <input
-                    type="checkbox"
-                    {...register(`check_${box}`)}
-                    className="accent-pink-600"
-                  />
-                  <span className="text-gray-800">{box}</span>
+            {/* Checkboxes for LDHC*/}
+            <div className="grid grid-cols-2 gap-2">
+              <div className="border-2 border-black-600 rounded-lg p-4">
+                <label className="inline-block px-2 py-1 rounded-full text-blue-900 font-semibold hover:bg-blue-300 transition">
+                  Select LDHC
                 </label>
-              ))}
+                {checkBoxesLDHC.map((box) => (
+                  <label key={box} className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      {...register(`check_${box}`)}
+                      onChange={handleChange}
+                      className="accent-pink-600"
+                      value="yes"
+                    />
+                    <span className="text-gray-800">{box}</span>
+                  </label>
+                ))}
+              </div>
+              {/* Checkboxes for Additional Checks*/}
+              <div className="border-2 border-black-600 rounded-lg p-4">
+                <label className="inline-block px-2 py-1 rounded-full text-blue-900 font-semibold hover:bg-blue-300 transition">
+                  Select Additional Checks
+                </label>
+                {checkBoxesChecks.map((box) => (
+                  <label key={box} className="flex items-center space-x-2">
+                    <input
+                      type="checkbox"
+                      {...register(`check_${box}`)}
+                      onChange={handleChange}
+                      className="accent-green-600"
+                    />
+                    <span className="text-gray-800">{box}</span>
+                  </label>
+                ))}
+              </div>
             </div>
           </div>
         </div>
